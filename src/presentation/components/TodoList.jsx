204bc872ece8e56861cc0ffb709b3f3fceb61831
CifronPro <ars132465@gmail.com>
2024-11-03 21:08:21 +0700

    add drag and drop

--- src/presentation/components/TodoList.jsx
+++ src/presentation/components/TodoList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import taskManager from "../../domain/TaskManager";
 import Task from "./Task";
 import TaskInput from "./TaskInput";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const TodoList = () => {
     const noTasks = document.querySelector('.no_tasks');
@@ -21,18 +22,51 @@ const TodoList = () => {
         setTasks([...tasks, newTask]);
     };
 
+    const handleOnDragEnd = (result) => {
+        const { source, destination } = result;
+
+        if (!destination) return; 
+
+        const reorderedTasks = Array.from(tasks);
+        const [movedTask] = reorderedTasks.splice(source.index, 1);
+        reorderedTasks.splice(destination.index, 0, movedTask);
+
+        setTasks(reorderedTasks);
+    };
+
+
     return (
-        <div className="todo_container">
-            <TaskInput onTaskCreated={onTaskCreated} />
-            {tasks.map(task => (
-                <Task
-                    key={task.id}
-                    title={task.title}
-                    about={task.about}
-                    id={task.id}
-                />
-            ))}
-        </div>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+        <TaskInput onTaskCreated={onTaskCreated} />
+        <Droppable droppableId="tasks">
+            {(provided) => (
+                <div
+                    className="todo_container"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                >
+                    {tasks.map((task, index) => (
+                        <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
+                            {(provided) => (
+                                <div
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                >
+                                    <Task
+                                        title={task.title}
+                                        about={task.about}
+                                        id={task.id}
+                                    />
+                                </div>
+                            )}
+                        </Draggable>
+                    ))}
+                    {provided.placeholder}
+                </div>
+            )}
+        </Droppable>
+    </DragDropContext>
     );
 };
 
